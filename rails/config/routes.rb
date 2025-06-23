4e91669c6f13970a26fd02af80ded7a166b5e9dd
yuta20253 <magee.20253@gmail.com>
2025-06-24 00:22:37 +0900

    add: ルーティングを設定

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -8,4 +9,31 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  namespace :api do
+    namespace :v1 do
+      devise_for :users,
+      path: "",
+      controllers: {
+        sessions: "api/v1/users/sessions",
+        registrations: "api/v1/users/registrations"
+      },
+      path_names: {
+        sign_in: "login",
+        sign_out: "logout",
+        registration: "signup"
+      }
+      # 診断履歴（マイページ）
+      resources :histories, only: [:index, :show, :create]
+      # 画像アップロード処理
+      resources :uploads, only: [:create]
+      # 診断結果（雑草→土壌→野菜）
+      resources :diagnoses, only: [:show]
+      # 野菜詳細情報
+      resources :vegetables, only: [:index, :show]
+      # 管理者用：知識ベース管理
+      namespace :admin do
+        resources :knowledges, only: [:index, :create, :update, :destroy]
+      end
+    end
+  end
 end
