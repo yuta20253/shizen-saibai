5c84212764d94a86e1d7ae67b8e6166ac89a8502
yuta20253 <magee.20253@gmail.com>
2025-06-24 10:04:51 +0900

    add: コントローラー作成

---
+++ rails/app/controllers/api/v1/users/registrations_controller.rb
@@ -0,0 +1,18 @@
+class API::V1::UsersRegistrationsController < Devise::RegistrationsController
+  respond_to :json
+
+  private
+
+  def respond_with(resource, _opts = {})
+    if resource.persisted?
+      render json: {
+        status: { code: 200, message: 'Signed up successfully.' },
+        data: resource
+      }, status: :ok
+    else
+      render json: {
+        status: { code: 422, message: "User couldn't be created successfully.", errors: resource.errors.full_messages }
+      }, status: :unprocessable_entity
+    end
+  end
+end
---
+++ rails/app/controllers/api/v1/vegetables_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::VegetablesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+end
