20fa62d71f61eeb591fb2b2ced1eff18b7f01f76
yuta20253 <magee.20253@gmail.com>
2025-06-28 19:10:06 +0900

    feat: Userのファクトリーデータを作成

--- rails/spec/factories/users.rb
+++ rails/spec/factories/users.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :user do
-    
+    name { "テストユーザー" }
+    role { 0 }
+    jti { SecureRandom.uuid }
+    email { Faker::Internet.unique.email }
+    password { "password" }
+    confirmed_at { Time.current }        # confirmable対応
+    confirmation_sent_at { Time.current }
+    unconfirmed_email { nil }            # メール変更保留用。不要ならnilでOK
   end
 end
