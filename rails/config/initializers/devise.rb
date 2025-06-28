ac1702ad7694227798a0128274196a6763becdb3
yuta20253 <magee.20253@gmail.com>
2025-06-28 19:25:19 +0900

    fix: Devise JWTの発行・無効化パスをAPIルーティング(/api/v1/...)に修正

--- rails/config/initializers/devise.rb
+++ rails/config/initializers/devise.rb
@@ -314,12 +314,11 @@ Devise.setup do |config|
   config.jwt do |jwt|
     jwt.secret = Rails.application.credentials.devise[:jwt_secret_key] || ENV['DEVISE_JWT_SECRET_KEY']
     jwt.dispatch_requests = [
-      ['POST', %r{^/login$}],
-      ['POST', %r{^/users/sign_in$}]
+      ['POST', %r{^/api/v1/login$}],
+      ['POST', %r{^/api/v1/signup$}],
     ]
     jwt.revocation_requests = [
-      ['DELETE', %r{^/logout$}],
-      ['DELETE', %r{^/users/sign_out$}]
+      ['DELETE', %r{^/api/v1/logout$}],
     ]
     jwt.expiration_time = 1.day.to_i
   end
