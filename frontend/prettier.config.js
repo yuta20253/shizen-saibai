01ebbb698cc4ee5b2eac2d0ad6126fd3d696081f
yuta20253 <magee.20253@gmail.com>
2025-07-07 09:58:31 +0900

    add: eslint prettier追加

---
+++ frontend/prettier.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  semi: true, // セミコロンを付ける
+  singleQuote: true, // シングルクォートを使う
+  trailingComma: "es5", // ES5で許される箇所にカンマを付ける
+  printWidth: 100, // 1行の最大文字数
+  tabWidth: 2, // インデント幅
+  arrowParens: "avoid", // 引数が1つのアロー関数の括弧省略
+};
