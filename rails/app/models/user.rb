6f8fde1b00537fc053196a1b1b1c88bf58de8b23
yuta20253 <magee.20253@gmail.com>
2025-07-09 14:21:05 +0900

    feat: Userモデルにバリデーションと退会の処理実装

--- rails/app/models/user.rb
+++ rails/app/models/user.rb
@@ -8,4 +8,33 @@ class User < ApplicationRecord
   include Devise::JWT::RevocationStrategies::JTIMatcher
 
   has_many :diagnoses
+
+  default_scope { where(deleted_at: nil) }
+
+  validates :name, presence: true
+  validates :email, presence: true,
+                    uniqueness: { scope: :deleted_at, message: 'は既に登録されています' },
+                    format: { with: URI::MailTo::EMAIL_REGEXP }
+
+
+  def generate_masked_email
+    loop do
+      email = "deleted_#{SecureRandom.hex(8)}@example.com"
+      break email unless User.exists?(email: email)
+    end
+  end
+  # 論理削除＋メールマスキング
+  def soft_delete!
+    update!(
+      deleted_at: Time.current,
+      email: generate_masked_email,
+      name: "********",
+      encrypted_password: Devise::Encryptor.digest(self.class, SecureRandom.hex(10))
+    )
+  end
+
+  # 退会ユーザーはログイン不可にする
+  def active_for_authentication?
+    super && deleted_at.nil?
+  end
 end
