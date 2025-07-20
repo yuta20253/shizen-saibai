19592922d9fd6cfec9408b749e6d55e6d7e107be
yuta20253 <magee.20253@gmail.com>
2025-07-20 21:19:44 +0900

    refactor: 適切な長さで改行

--- frontend/components/Auth/Login/index.tsx
+++ frontend/components/Auth/Login/index.tsx
@@ -9,26 +9,67 @@ type UserForm = {
 };
 
 export const LoginForm = (): React.JSX.Element => {
-  const { register, handleSubmit, formState: { errors } } = useForm<UserForm>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<UserForm>();
   const onSubmit: SubmitHandler<UserForm> = (data: UserForm) => {
     console.log(data);
   };
   return (
     <Box sx={{ padding: 2, width: '100%' }}>
-      <Box component="form" sx={{ width: '100%', maxWidth: 600, mx: 'auto', mt: 5 }} onSubmit={handleSubmit(onSubmit)}>
+      <Box
+        component="form"
+        sx={{ width: '100%', maxWidth: 600, mx: 'auto', mt: 5 }}
+        onSubmit={handleSubmit(onSubmit)}
+      >
         <Box sx={{ mb: 2 }}>
           <Typography>メールアドレス</Typography>
-          <TextField fullWidth variant="outlined" {...register('email', { required: 'メールアドレスを入力してください', pattern: { value: /^[\w.-]+@[\w.-]+\.[A-Za-z]{2,}$/, message: 'メールアドレスの形式が正しくありません'} })} error={!!errors.email} helperText={errors.email?.message} />
+          <TextField
+            fullWidth
+            variant="outlined"
+            {...register('email', {
+              required: 'メールアドレスを入力してください',
+              pattern: {
+                value: /^[\w.-]+@[\w.-]+\.[A-Za-z]{2,}$/,
+                message: 'メールアドレスの形式が正しくありません',
+              },
+            })}
+            error={!!errors.email}
+            helperText={errors.email?.message}
+          />
         </Box>
         <Box sx={{ mb: 2 }}>
           <Typography>パスワード</Typography>
-          <TextField type='password' fullWidth variant="outlined" {...register('password', { required: 'パスワードを入力してください', minLength: { value: 8, message: '8文字以上で入力してください' } })} error={!!errors.password} helperText={errors.password?.message} />
+          <TextField
+            type="password"
+            fullWidth
+            variant="outlined"
+            {...register('password', {
+              required: 'パスワードを入力してください',
+              minLength: { value: 8, message: '8文字以上で入力してください' },
+            })}
+            error={!!errors.password}
+            helperText={errors.password?.message}
+          />
         </Box>
         <Box sx={{ my: 4 }}>
-          <Button type='submit' sx={{ width: '100%', backgroundColor: '#000000', color: '#ffffff', p: 2, fontSize: 'large' }}>ログイン</Button>
+          <Button
+            type="submit"
+            sx={{
+              width: '100%',
+              backgroundColor: '#000000',
+              color: '#ffffff',
+              p: 2,
+              fontSize: 'large',
+            }}
+          >
+            ログイン
+          </Button>
         </Box>
         <Box sx={{ width: '100%', textAlign: 'center' }}>
-          <Link href="/">パスワードをお忘れの方はこちら</Link>
+          <Link href="/password-reset">パスワードをお忘れの方はこちら</Link>
         </Box>
       </Box>
     </Box>
