3997f0c16e31a5bd6ce430efb166dc870a34f895
yuta20253 <magee.20253@gmail.com>
2025-07-21 23:33:18 +0900

    fix: ページによってフッターの表示を変更

--- frontend/components/Footer/index.tsx
+++ frontend/components/Footer/index.tsx
@@ -3,42 +3,51 @@
 import { Box, IconButton } from '@mui/material';
 import React from 'react';
 import CameraAltIcon from '@mui/icons-material/CameraAlt';
+import { usePathname } from 'next/navigation';
 
-export const Footer = (): React.JSX.Element => (
-  <Box
-    component="footer"
-    sx={{
-      position: 'fixed',
-      bottom: 0,
-      left: 0,
-      width: '100%',
-      height: 48,
-      bgcolor: 'background.paper',
-      boxShadow: '0 -2px 8px rgba(0,0,0,0.1)',
-      display: 'flex',
-      justifyContent: 'center',
-      px: 4,
-      zIndex: 9999,
-    }}
-  >
-    <IconButton
-      size="large"
+export const Footer = (): React.JSX.Element | null => {
+  const pathName = usePathname();
+  const hideAuthButtonPaths = ['/login', '/signup'];
+  const showAuth = !hideAuthButtonPaths.includes(pathName);
+
+  if (!showAuth) return null;
+
+  return (
+    <Box
+      component="footer"
       sx={{
-        position: 'absolute',
-        top: -28,
-        left: '50%',
-        transform: 'translateX(-50%)',
+        position: 'fixed',
+        bottom: 0,
+        left: 0,
+        width: '100%',
+        height: 48,
         bgcolor: 'background.paper',
-        width: 56,
-        height: 56,
-        borderRadius: '50%',
-        boxShadow: 3,
-        '&:hover': {
-          bgcolor: 'background.paper',
-        },
+        boxShadow: '0 -2px 8px rgba(0,0,0,0.1)',
+        display: 'flex',
+        justifyContent: 'center',
+        px: 4,
+        zIndex: 9999,
       }}
     >
-      <CameraAltIcon sx={{ fontSize: '1.8rem' }} />
-    </IconButton>
-  </Box>
-);
+      <IconButton
+        size="large"
+        sx={{
+          position: 'absolute',
+          top: -28,
+          left: '50%',
+          transform: 'translateX(-50%)',
+          bgcolor: 'background.paper',
+          width: 56,
+          height: 56,
+          borderRadius: '50%',
+          boxShadow: 3,
+          '&:hover': {
+            bgcolor: 'background.paper',
+          },
+        }}
+      >
+        <CameraAltIcon sx={{ fontSize: '1.8rem' }} />
+      </IconButton>
+    </Box>
+  );
+};
