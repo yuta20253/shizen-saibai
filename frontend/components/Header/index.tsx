12b001751302c295eb1284d6f677007317e5b78b
centerRyo <abc.atrn@gmail.com>
2025-07-23 22:36:50 +0900

    SPA遷移させる

--- frontend/components/Header/index.tsx
+++ frontend/components/Header/index.tsx
@@ -6,6 +6,7 @@ import Toolbar from '@mui/material/Toolbar';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import { useAuth } from '@/context/AuthContext';
 import { usePathname } from 'next/navigation';
+import Link from 'next/link';
 
 export const Header = (): React.JSX.Element => {
   const { user } = useAuth();
@@ -20,22 +21,39 @@ export const Header = (): React.JSX.Element => {
       <AppBar position="fixed">
         <Toolbar>
           <Box sx={{ flexGrow: 1 }}>
-            <Button component="a" href="/" sx={{ backgroundColor: '#013220', color: '#ffffff' }}>
+            <Link
+              href="/"
+              style={{
+                display: 'inline-flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                backgroundColor: '#013220',
+                color: '#ffffff',
+                padding: '6px 16px',
+                borderRadius: '4px',
+                textDecoration: 'none',
+                fontSize: '0.875rem',
+                fontWeight: 500,
+                lineHeight: 1.75,
+                cursor: 'pointer',
+                width: '64px',
+              }}
+            >
               ロゴ
-            </Button>
+            </Link>
           </Box>
           {showAuth ? (
             user ? (
               <Box sx={{ display: 'flex', gap: 2 }}>
                 <AccountCircleIcon />
-                {user ? user.name : 'ユーザー情報取得中'}
+                {user.name}
               </Box>
             ) : (
               <Box sx={{ display: 'flex', gap: 2 }}>
-                <Button color="inherit" variant="outlined" href="/login">
+                <Button LinkComponent={Link} color="inherit" variant="outlined" href="/login">
                   ログイン
                 </Button>
-                <Button color="inherit" variant="outlined">
+                <Button LinkComponent={Link} color="inherit" variant="outlined">
                   新規登録
                 </Button>
               </Box>
