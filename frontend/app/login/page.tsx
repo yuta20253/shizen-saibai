70e59c2a40efdd68f3d2fe984bdd141afcf253a7
yuta20253 <magee.20253@gmail.com>
2025-07-23 14:32:34 +0900

    fix: 見出しの大きさを変更

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -6,7 +6,7 @@ import { Box, Typography } from '@mui/material';
 const LoginPage = (): React.JSX.Element => {
   return (
     <Box sx={{ padding: 2, maxWidth: '960px', width: '100%', margin: '0 auto' }}>
-      <Typography variant="h3" component="p" textAlign="center" sx={{ fontWeight: 'bold', mt: 4 }}>
+      <Typography variant="h4" component="p" textAlign="center" sx={{ fontWeight: 'bold', mt: 4 }}>
         ログイン
       </Typography>
       <LoginForm />
