0acebb93491262ae5049e1575b27447590476efb
yuta20253 <magee.20253@gmail.com>
2025-07-31 00:00:53 +0900

    fix: useStateを削除

--- frontend/components/RequireAuth/index.tsx
+++ frontend/components/RequireAuth/index.tsx
@@ -2,21 +2,19 @@
 
 import { useAuth } from '@/context/AuthContext';
 import { useRouter } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 
 export const RequireAuth = ({ children }: { children: React.ReactNode }) => {
-  const [token, setToken] = useState<string | null>(null);
   const { user, loading } = useAuth();
   const router = useRouter();
   useEffect(() => {
     const storedToken = localStorage.getItem('token');
-    setToken(storedToken);
     if (!loading && (!user || !storedToken)) {
       router.replace('/login');
     }
-  }, [user, router, token, loading]);
+  }, [user, router, loading]);
 
-  if (user === null || token === null || loading) return null;
+  if (user === null || loading) return null;
 
   return <>{children}</>;
 };
