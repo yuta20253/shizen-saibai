62b722b54bb3eb17cc63c9ad7ecbd8569b696ebc
yuta20253 <magee.20253@gmail.com>
2025-07-30 13:42:19 +0900

    add: 仮の遷移先を準備

---
+++ frontend/app/mypage/delete/page.tsx
@@ -0,0 +1,5 @@
+const DeleteUser = (): React.JSX.Element => {
+  return <>退会画面</>;
+};
+
+export default DeleteUser;
---
+++ frontend/app/mypage/edit/page.tsx
@@ -0,0 +1,5 @@
+const EditUser = (): React.JSX.Element => {
+  return <>ユーザー情報編集画面</>;
+};
+
+export default EditUser;
