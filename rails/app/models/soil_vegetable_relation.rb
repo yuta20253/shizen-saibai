06772915f84013a808b203ce13994bd4383d1f4c
yuta20253 <magee.20253@gmail.com>
2025-07-31 23:21:33 +0900

    feat: SoilVegetableRelationモデルにenumカラムを実装

--- rails/app/models/soil_vegetable_relation.rb
+++ rails/app/models/soil_vegetable_relation.rb
@@ -1,4 +1,10 @@
 class SoilVegetableRelation < ApplicationRecord
   belongs_to :soil
   belongs_to :vegetable
+
+  enum suitability: {
+    high: 0, # 高
+    middle: 1, # 中
+    low: 2 # 低
+  }
 end
