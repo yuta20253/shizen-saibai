8be3991650325973a6b828db5b5a2ca68b0b009e
yuta20253 <magee.20253@gmail.com>
2025-08-03 16:42:02 +0900

    feat: イメージ画像のテストデータを用意・PromptResponderService内に対してのエラーハンドリング実装

--- rails/app/controllers/api/v1/diagnoses_controller.rb
+++ rails/app/controllers/api/v1/diagnoses_controller.rb
@@ -3,9 +3,8 @@ class Api::V1::DiagnosesController < Api::V1::BaseController
   def create
     file_type = ['image/jpeg', 'image/png']
     # max_size = 5.megabytes
-
-    image = "https://www.google.com/search?sca_esv=87f102440c8210d8&sxsrf=AE3TifP6FJodp3pln1k3DnACt9XSfM6fXA:1754176147332&udm=2&fbs=AIIjpHzQki16q-8Z7j6aseYi2jA_awT46aeCpCIps-CmKmRFQnye5ZCZ-joo3KzReil4N40QrqWZdLtqYZcrC12sd5RRjC38EHLHO6A0CnHavpwLTiJ-ckXqcav9y-eXIg8IfNrI9XUpM3ALEobobagXOJglX0WFqzRZJrdgpmiIZOuVOPMHVXZPOINQcNjGQlOqPuh7rA56sWmDiUasxyzE8KAJSNi9j-46SgWI1P1oirZJjUMinc0poFVM2CYe5iWxXeTuYt6A&q=%E3%82%B9%E3%82%BA%E3%83%A1%E3%83%8E%E3%82%AB%E3%82%BF%E3%83%93%E3%83%A9&sa=X&ved=2ahUKEwipnYqxn-2OAxVEna8BHUBGEzUQtKgLegQIERAB&biw=1440&bih=812&dpr=2#vhid=UyneVnNBtatKlM&vssid=mosaic"
-
+    base64_image = File.read(Rails.root.join('lib', 'diagnosis_image.txt')).strip
+    image = 'data:image/jpeg;base64,' + base64_image
     # ファイル形式が不正
     # raise ActionController::ParameterMissing, :image if params[:image].blank?
     # unless file_type.include?(image.content_type)
@@ -26,10 +25,12 @@ class Api::V1::DiagnosesController < Api::V1::BaseController
 
     begin
       data = Diagnosis::Ai::PromptResponderService.new(vegetables_json: vegetables_json, weeds_json: weeds_json, soils_json: soils_json, image: image).call
-    rescue => e
-      Rails.logger.error("OpenAI呼び出しエラー: #{e.class} - #{e.message}")
-      Rails.logger.error(e.backtrace.join("\n"))
-      render json: { error: e.message }, status: 500 and return
+    rescue Diagnosis::Errors::RateLimitExceeded => e
+      render json: { error: "OpenAIの利用制限を超えました" }, status: 429
+    rescue Diagnosis::Errors::InvalidResponseFormat => e
+      render json: { error: "OpenAIの応答が不正です: #{e.message}" }, status: 400
+    rescue Diagnosis::Errors::OpenAiCallFailed => e
+      render json: { error: "OpenAI呼び出しエラー: #{e.message}" }, status: 500
     end
 
     vegetable_name = data["vegetable"]["name"]
