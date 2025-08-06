d9f1cc52dd9f7835a59a82eb2ef8a18bd3b3f61c
yuta20253 <magee.20253@gmail.com>
2025-08-07 00:19:57 +0900

    fix: 一旦ファイル関係の処理は消した

--- rails/app/controllers/api/v1/diagnoses_controller.rb
+++ rails/app/controllers/api/v1/diagnoses_controller.rb
@@ -2,21 +2,8 @@ class Api::V1::DiagnosesController < Api::V1::BaseController
   require 'openai'
   def create
     file_type = ['image/jpeg', 'image/png']
-    # max_size = 5.megabytes
     base64_image = File.read(Rails.root.join('lib', 'diagnosis_image1.txt')).strip
     image = 'data:image/jpeg;base64,' + base64_image
-    # ファイル形式が不正
-    # raise ActionController::ParameterMissing, :image if params[:image].blank?
-    # unless file_type.include?(image.content_type)
-    #   render json: {message: "ファイル形式が不正です", code: :invalid_file_type, status: "error"}
-    #   return
-    # end
-
-    # ファイルサイズ超過
-    # if image.size > max_size
-    #   render json: { status: 'error', code: :file_size_exceeded, message: 'ファイルサイズが大きすぎます' }, status: :bad_request
-    #   return
-    # end
 
     data_json = Diag::Json::JsonExportService.new.call
     weeds_json = data_json[:weeds_to_json]
