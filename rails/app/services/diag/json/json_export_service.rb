1f7ebb80cdae1ab7c8aee373aa879fe2e453dd94
yuta20253 <magee.20253@gmail.com>
2025-08-07 13:37:24 +0900

    feat: Diag::Json::JsonExportServiceのreturnをnameだけに変更

--- rails/app/services/diag/json/json_export_service.rb
+++ rails/app/services/diag/json/json_export_service.rb
@@ -3,11 +3,8 @@ class Diag::Json::JsonExportService
     weeds = Weed.all.map do |weed|
       {
         name: weed.name,
-        scientific_name: weed.scientific_name,
-        image_url: weed.image_url,
-        description: weed.description
       }
     end
-    return { weeds_to_json: weeds.to_json }
+    return { weeds_name_to_json: weeds.to_json }
   end
 end
