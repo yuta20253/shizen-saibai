fb19d983e99cfe5de9ad280e2fe28984d19eface
yuta20253 <magee.20253@gmail.com>
2025-08-09 18:21:44 +0900

    feat: indexアクションを実装(診断結果一覧を返す処理)

--- rails/app/controllers/api/v1/histories_controller.rb
+++ rails/app/controllers/api/v1/histories_controller.rb
@@ -1,5 +1,11 @@
 class Api::V1::HistoriesController < Api::V1::BaseController
   def index
+    diagnoses = current_user.diagnoses.includes(:weed, :soil, :vegetable)
+    if diagnoses.exists?
+      render json: diagnoses, each_serializer: ::DiagnosisSerializer
+    else
+      render json: { message: "診断履歴がありません。" }
+    end
   end
 
   def show
