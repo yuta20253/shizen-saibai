44ba8eacc1128d7e1e33fb953607888599422742
yuta20253 <magee.20253@gmail.com>
2025-08-13 17:02:11 +0900

    fix: 診断結果一覧画面でもカメラアイコンが表示されるように変更

--- frontend/components/Footer/index.tsx
+++ frontend/components/Footer/index.tsx
@@ -7,7 +7,7 @@ import { usePathname } from 'next/navigation';
 
 export const Footer = (): React.JSX.Element | null => {
   const pathName = usePathname();
-  const hiddenPaths = ['/login', '/signup', '/mypage/diagnoses'];
+  const hiddenPaths = ['/login', '/signup'];
   const hidden = hiddenPaths.includes(pathName);
 
   if (hidden) return null;
