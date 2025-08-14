2afa3021c6652a6c351a8fff8e750fe0d41e2c0d
yuta20253 <magee.20253@gmail.com>
2025-08-14 23:43:59 +0900

    fix: コンポーネントの変数名変更

--- frontend/app/mypage/diagnoses/[id]/page.tsx
+++ frontend/app/mypage/diagnoses/[id]/page.tsx
@@ -1,5 +1,5 @@
-import { DiagnosisDetail } from '@/features/Diagnosis';
+import { Diagnosis } from '@/features/Diagnosis';
 
-const DiagnosisPage = (): React.JSX.Element => <DiagnosisDetail />;
+const DiagnosisPage = (): React.JSX.Element => <Diagnosis />;
 
 export default DiagnosisPage;
