62c9257611cab2da9e3af8d10efa2bdc6041559a
yuta20253 <magee.20253@gmail.com>
2025-08-28 23:19:06 +0900

    fix: envを使用する形に修正

--- rails/config/application.rb
+++ rails/config/application.rb
@@ -39,6 +39,7 @@ module Myapp
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
+    config.secret_key_base = ENV["SECRET_KEY_BASE"]
     config.api_only = true
     config.i18n.default_locale = :ja
     config.enable_reloading = true
