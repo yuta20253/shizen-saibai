7a8b9e43064a216330ff6390ebd448c1c659f2d7
yuta20253 <magee.20253@gmail.com>
2025-09-13 00:20:22 +0900

    add: UserMailer実装

---
+++ rails/app/mailers/user_mailer.rb
@@ -0,0 +1,15 @@
+class UserMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_mailer.reset_password_email.subject
+  #
+  def reset_password(user, token)
+    @user = user
+    @token = token
+    url = "#{ENV["FRONTEND_URL"]}/password/reset/#{@token}?email=#{@user.email}"
+
+    mail(to: @user.email, subject: "パスワードリセットのご案内", body: "パスワードリセットはこちらのリンクからお願いします： #{url}")
+  end
+end
