329c7471b7d14cf5a792e728cd7551b0497b4f1b
yuta20253 <magee.20253@gmail.com>
2025-09-23 14:44:59 +0900

    refactor: Rubocopの適用

--- rails/app/controllers/api/v1/password_resets_controller.rb
+++ rails/app/controllers/api/v1/password_resets_controller.rb
@@ -18,20 +18,10 @@ class Api::V1::PasswordResetsController < ApplicationController
   end
 
   def update
-    return render_user_not_found unless @user
-    return render_invalid_token unless token_valid?
-    return render_expired_token unless @user.reset_password_period_valid?
-
-    if params[:password].blank? || params[:password_confirmation].blank?
-      return render json: { message: "パスワードと確認用パスワードを入力してください。" }, status: :unprocessable_content
-    end
-
-    if params[:password] != params[:password_confirmation]
-      return render json: { message: "パスワードが一致しません。" }, status: :unprocessable_content
-    end
+    return unless validate_update_request?
 
     if @user.update(password: params[:password])
-      @user.update(reset_password_token: nil, reset_password_sent_at: nil)
+      @user.update!(reset_password_token: nil, reset_password_sent_at: nil)
 
       render json: { message: "パスワードを更新しました。" }, status: :ok
     else
@@ -40,6 +30,7 @@ class Api::V1::PasswordResetsController < ApplicationController
   end
 
   private
+
     def find_user
       @user = User.find_by(email: params[:email])
     end
@@ -47,8 +38,35 @@ class Api::V1::PasswordResetsController < ApplicationController
     def token_valid?
       token = params[:token]
       return false if token.blank?
+
       digest_token = Devise.token_generator.digest(User, :reset_password_token, token)
       @user.reset_password_token == digest_token
+    end
+
+    def validate_update_request?
+      return render_user_not_found && false unless @user
+      return render_invalid_token && false unless token_valid?
+      return render_expired_token && false unless @user.reset_password_period_valid?
+      return render_password_or_password_confirmation_blank && false unless password_present?
+      return render_mismatch_password && false unless passwords_match?
+
+      true
+    end
+
+    def password_present?
+      params[:password].present? && params[:password_confirmation]
+    end
+
+    def passwords_match?
+      params[:password] == params[:password_confirmation]
+    end
+
+    def render_password_or_password_confirmation_blank
+      render json: { message: "パスワードと確認用パスワードを入力してください。" }, status: :unprocessable_content
+    end
+
+    def render_mismatch_password
+      render json: { message: "パスワードが一致しません。" }, status: :unprocessable_content
     end
 
     def render_user_not_found
