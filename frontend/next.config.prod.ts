c890d85a4228a18e17956a8eadbe72bfcb5a69bd
yuta20253 <magee.20253@gmail.com>
2025-11-04 22:55:07 +0900

    .dockerignoreとnext.config.prod.ts追加

---
+++ frontend/next.config.prod.ts
@@ -0,0 +1,15 @@
+// frontend/next.config.ts
+import path from 'path';
+import { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  webpack(config) {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    config.resolve.alias['@components'] = path.resolve(__dirname, 'components');
+    config.resolve.alias['@features'] = path.resolve(__dirname, 'features');
+    config.resolve.alias['@context'] = path.resolve(__dirname, 'context');
+    return config;
+  },
+};
+
+export default nextConfig;
