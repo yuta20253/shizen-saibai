a55526768ac6026f6d06500cad7405925668d4f6
yuta20253 <magee.20253@gmail.com>
2025-11-18 20:39:11 +0900

    corsのoriginを修正

--- rails/config/initializers/cors.rb
+++ rails/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000", "http://163.43.209.197"
+    origins "http://localhost:3000", "http://163.43.209.197", 'http://zasso-lens.com'
 
     resource "*",
       headers: :any,
