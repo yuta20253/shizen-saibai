240ff645fe07abdab2f5a3e0dcf08e93881d2a01
centerRyo <abc.atrn@gmail.com>
2025-11-28 13:34:11 +0900

    モジュール解決

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from 'next';
+import path from 'path';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config) {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    config.resolve.alias['@components'] = path.resolve(__dirname, 'components');
+    config.resolve.alias['@features'] = path.resolve(__dirname, 'features');
+    config.resolve.alias['@context'] = path.resolve(__dirname, 'context');
+    return config;
+  },
 };
 
 export default nextConfig;
